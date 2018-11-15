aec5c98f283f05f6e68d90d0f1ddae194eda0042
wallentx <william.allentx@gmail.com>
2018-11-15 17:25:13 -0600

    typo

--- s3tree.go
+++ s3tree.go
@@ -7,7 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/wallentx/tree"
+	"github.com/wallentx/s3tree"
 	"os"
 )
 
